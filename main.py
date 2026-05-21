532754606b4674b7b4b7dff163bf9dfed7fbc987
Henri Kähkönen <henri.kahkonen@gmail.com>
2026-05-21 08:34:32 +0300

    Rewrote logic to not care about directory type when copying. Added file and dir copy functionality and some metadata calculations on operation itself

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import sys, argparse, time, json, os
+import sys, argparse, time, json, os, math
+from datetime import datetime
 import ffmpeg
 import utils.exceptions as excepts
 from dotenv import dotenv_values
 from pathlib import Path
 
-from utils.funcs import isAudioFile
+from utils.funcs import isAudioFile, getExtension, calculateNewAverage, copyFileToTarget
 from utils.constants import ffmpeg_audiocontainers
 
 def main(argv: list[str]) -> int:
@@ -18,11 +19,13 @@ def main(argv: list[str]) -> int:
     logging = args.log
     if logging:
         print("Logging: True")
-        #TODO: implement    
+        #TODO: implement logging
     
     #  ================
     #  Pre-operation validations
     #  ================
+
+    timetostartpreparations = datetime.now()
 
     print("Reading and validating .env values...")
     configjson = json.loads(json.dumps(dotenv_values(".env")))
@@ -42,68 +45,75 @@ def main(argv: list[str]) -> int:
         if (targetcontainer not in ffmpeg_audiocontainers):
             raise excepts.InvalidFileFormatException(f"Target file format \"{targetcontainer}\" is not an audio file format supported by ffmpeg.")
     
-        sourcedir_folders = [ name for name in os.listdir(sourcedir) if os.path.isdir(os.path.join(sourcedir, name)) ]
-        audiofilesfound = False
-        for (root,dirs,files) in os.walk(sourcedir):
-            #print(" ")
-            #print(f"Root: {root}")
-            #print(f"Dirs: {dirs}")
-            #print(f"File: {file}")
-            #time.sleep(3)
+        audiofilestoconvert = 0
+        audiofilestomove = 0
+        for (sourcepath, dirs, files) in os.walk(sourcedir):
             for file in files:
                 if isAudioFile(file):
-                    audiofilesfound = True
-                    break
-            if audiofilesfound:
-                break
-        if not audiofilesfound:
+                    if getExtension(file) == targetcontainer:
+                        audiofilestomove+=1
+                    else:
+                        audiofilestoconvert+=1
+        if audiofilestoconvert+audiofilestomove==0:
             raise excepts.InvalidDirectoryException(f"Directory \"{sourcedir}\" doesn't contain any audio files!")
     
     except Exception as e:
         print(f"There was an error with the values in the .env file: {e}\nCheck your .env file.")
         raise SystemExit(1)
     
-    print(".env values validated. Beginning to process files...\n")
-    
+    print(".env values validated.")
+    print(f"Found a total of {audiofilestoconvert+audiofilestomove} audio files, {audiofilestomove} of which are already in the desired target format.")
+    timedelta = datetime.now() - timetostartpreparations
+    print(f"Preparations took {timedelta.microseconds/1000000} seconds.\n")
+        
     # =====================
     # File conversion loop
     # =====================
 
+    print(f"Starting to convert files...\n")
+
+    timetostartfullprocess = datetime.now()
+    fileintervaltoprint = math.ceil(int(audiofilestoconvert/100)) # Give 100 status updates to the user during the process
+    filesconverted = 0
+    averagefileconversionlength = 0 
+
     for (sourcepath, dirs, files) in os.walk(sourcedir):
-        # Inspect whether or not dir contains audio files
-        isMusicFolder = False
-        if files:
-            for file in files:
-                if isAudioFile(file):
-                    isMusicFolder = True
-                    break
-            if isMusicFolder:
-                targetpath = Path(sourcepath.replace(sourcedir,targetdir))
-                try:
-                    print(targetpath)
-                    os.makedirs(targetpath)
-                except FileExistsError:
-                    print(f"Directory \"{targetpath}\" already exists.")
-                except PermissionError:
-                    print(f"Permission denied: cannot create target path \"{targetpath}\"")
-                    raise
-                except Exception as e:
-                    print(f"Uncaught error occurred: {e}")
-                    raise
-                #print(targetpath)
-                #print("create")
 
+        #Copy each encountered folder to target
+        targetpath = Path(sourcepath.replace(sourcedir,targetdir))
+        try:
+            os.makedirs(targetpath)
+        except FileExistsError:
+            pass
+            #print(f"Directory \"{targetpath}\" already exists.")
+        except PermissionError:
+            print(f"Permission denied: cannot create target path \"{targetpath}\".")
+            raise
+        except Exception as e:
+            print(f"Uncaught error occurred: {e}")
+            raise
 
-    # TODO: function that processes every file and converts to .mp3
+        for file in files:
+            fullpath = os.path.join(sourcepath,file)
+            targetpath = fullpath.replace(sourcedir,targetdir)
+            if not isAudioFile(file) or getExtension(file) == targetcontainer:
+                copyFileToTarget(fullpath,targetpath)
+                continue
 
-    
+            conversionstartTime = datetime.now()
+            #convert and copy to target
+            conversiontime = (datetime.now() - conversionstartTime).microseconds
+            filesconverted += 1
+            averagefileconversionlength = calculateNewAverage(averagefileconversionlength,filesconverted,conversiontime)
 
-    
+            if filesconverted > 0 and filesconverted%fileintervaltoprint==0:
+                conversiontimethusfar = (datetime.now() - timetostartfullprocess).seconds
+                remainingconversions = audiofilestoconvert - filesconverted
+                estimatedremainingmicroseconds = remainingconversions*averagefileconversionlength
+                percentageready = float(filesconverted/audiofilestoconvert)
+                print(f"Converted {percentageready}% of files in {float(conversiontimethusfar/60)} minutes. Estimated time remaining: {float(estimatedremainingmicroseconds/1000000/60)} minutes.")
 
-
-    #except SyntaxError:
-    #    print("Error: .env file is malformed. Check that your .env file is set up correctly.")
-    #    sys.exit(1)
+                
 
 if __name__ == "__main__" :
     main(sys.argv)
--- utils/funcs.py
+++ utils/funcs.py
@@ -1,5 +1,7 @@
 from utils.constants import ffmpeg_audiocontainers
 import os
+from pathlib import Path
+import shutil
 
 def isAudioFile(file) -> bool:
     """Returns True or False depending on whether or not the file's extension is found in the ffmpeg audio container file extensions."""
@@ -7,3 +9,29 @@ def isAudioFile(file) -> bool:
     if file_extension in ffmpeg_audiocontainers:
         return True
     return False
+
+def getExtension(filename) -> str:
+    """Returns the file extension of file"""
+    return Path(filename).suffix
+
+def calculateNewAverage(oldaverage, instances, newvalue):
+    oldvaluestotal = oldaverage*instances
+    newvaluestotal = oldvaluestotal + newvalue
+    return newvaluestotal / (instances+1)
+
+def copyFileToTarget(sourcepath, targetpath):
+    """_summary_
+
+    Args:
+        sourcepath (str): Full path of source file
+        targetpath (str): Full path of target file
+
+    Raises:
+        IOError: If target path isn't writable
+    """
+    if os.path.isfile(sourcepath):
+        try:
+            shutil.copy2(sourcepath,targetpath)
+        except IOError as e:
+            print(f"Error: {e}")
+            raise IOError
